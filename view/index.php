96197c1a011a0b2c2bf512e68b34ce16ef961476
nyds <simego@hotmail.it>
2018-08-30 00:15:41 +0200

    Update index.php view

--- view/index.php
+++ view/index.php
@@ -5,7 +5,7 @@
 // These lines are for DEVELOPMENT only.  You should never display errors
 // in a production environment.
 error_reporting(E_ALL);
-ini_set( 'display_errors','1');
+ini_set('display_errors', '1');
 
 // import autoloader
 include '../Autoloader.php';
@@ -14,18 +14,18 @@ include '../Autoloader.php';
 
 <html>
 
-<head>
-    <meta charset="utf-8" />
-    <title>Prova</title>
-</head>
+    <head>
+        <meta charset="utf-8" />
+        <title>Prova</title>
+    </head>
 
-<body>
+    <body>
 
-    <form action="main.php" method="post">
-        Digita il nome di un autore o di un libro <input type="text" name="author"><br>
-        <input type="submit">
-    </form>
+        <form action="main.php" method="post">
+            Digita il nome di un autore o di un libro <input type="text" name="author"><br>
+            <input type="submit">
+        </form>
 
-</body>
+    </body>
 
 </html>
