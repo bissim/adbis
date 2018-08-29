862d8adc62f9d79817a91efaefbaa92391dc5988
nyds <simego@hotmail.it>
2018-08-29 02:39:56 +0200

    Minor edits to old wrappers

--- wrappers/amazon.php
+++ wrappers/amazon.php
@@ -1,157 +1,138 @@
 <?php
 
-//La classe book ha come variabili d'istanza i parametri di un libro, ne possiamo aggiungere altri successivamente
+namespace wrappers;
 
-class Book
+require '..\model\Book.php';
+
+use \model\Book;
+use \DOMDocument;
+use \DOMXPath;
+
+function getAmazonBooks(string $keyword): array
 {
-        private $titolo;
-        private $autore;
-        private $prezzo;
-        private $img;
-        private $link;
-        private $editore;
 
-//Costruttore
-        public function __construct($t, $a, $p, $i, $l, $e)
+        // L'URL della pagina dove appare il catalogo degli ebook cercati in base a una keyword
+        $amazonURL = 'https://www.amazon.it/s/ref=nb_sb_noss?__mk_it_IT=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=node%3D827182031&field-keywords=';
+        $keyword = str_replace(" ", "+", $keyword);
+        $urlSearch = $amazonURL . $keyword;
+
+        // La pagine viene caricate
+        $page = file_get_contents($urlSearch);
+
+        // Si crea un DOM della pagina e un XPath
+        $dom = new DOMDocument;
+        $dom->loadHTML($page);
+        $xpath = new DOMXPath($dom);
+
+        // La query per ottenere il link di ogni ebook dal catalogo mostrato
+        $query = '//div[@class="a-row a-spacing-small"]/div[@class="a-row a-spacing-none"]/a/attribute::href';
+
+        // Si estraggono i link
+        $entries = $xpath->query($query);
+
+        //echo $entriesLink->length . "<br>";
+
+        // Per ogni ebook nel catalogo si estrae il link della sua pagina di acquisto e lo si inserisce in un array
+        $links = array();
+        // foreach ($entries as $entryLink)
+        for ($i = 0; $i < 1; $i++) // TODO test ebook retrieval
         {
-        $this->titolo = $t;
-        $this->autore = $a;
-        $this->prezzo = $p;
-        $this->img = $i;
-        $this->link = $l;
-        $this->editore = $e;
+                // $l = $entryLink->firstChild->nodeValue;
+                $l = $entries[$i]->firstChild->nodeValue;
+                array_push($links, $l);
         }
 
-//Una sorta di toString
-        public function call()
-        {
-        echo "titolo: " . ($this->titolo) . "<br> autore: " . ($this->autore) . "<br> prezzo: " . ($this->prezzo) .
-                "<br>immmagine: " . ($this->img) . "<br>link: " . ($this->link). "<br>editore: " . ($this->editore) . "<hr>";
-        }
+        $books = searchBooks($links);
 
+        return $books;
 }
 
-function getAmazonBooks($keyword)
-{
-
-//L'URL della pagina dove appare il catalogo degli ebook cercati in base a una keyword
-$urlSearch = str_replace(" ", "+", "https://www.amazon.it/s/ref=nb_sb_noss?__mk_it_IT=%C3%85M%C3%85%C5%BD%C3%95%C3%91&url=node%3D827182031&field-keywords=" . $keyword);
-
-//La pagine viene caricate
-$page = file_get_contents($urlSearch);
-
-//Si crea un DOM della pagina e un xpath
-$DOM = new DOMDocument;
-$DOM->loadHTML($page);
-$xpath = new DOMXPath($DOM);
-
-//La query per ottenere il link di ogni ebook dal catalogo mostrato
-$queryLink = '//div[@class="a-row a-spacing-small"]/div[@class="a-row a-spacing-none"]/a/attribute::href';
-
-//Si estraggono i link
-$entriesLink = $xpath->query($queryLink);
-
-//echo $entriesLink->length . "<br>";
-
-//Per ogni ebook nel catalogo si estrae il link della sua pagina di acquisto e lo si inserisce in un array
-$links = array();
-foreach ($entriesLink as $entryLink)
-{
-        $l =  $entryLink->firstChild->nodeValue;
-        array_push($links, $l);
-}
-
-$books = searchBooks($links);
-
-return $books;
-}
-
-//L'array dei libri
+// L'array dei libri
 //$books = array();
 
-function searchBooks($links)
+function searchBooks(array $links): array
 {
 
-$booksFound = array();
+        $booksFound = array();
 
-//Per ogni link corrispondente ad un ebook
-foreach ($links as $link)
-{
-	//Bisogna estrarre i parametri dell'ebook nella sua pagina
-	$pageItem = file_get_contents($link);
-	$DOMItem = new DOMDocument();
-        $DOMItem->loadHTML($pageItem);
-        $xpathItem = new DOMXPath($DOMItem);
-
-	//La query per ottenere il titolo
-        $queryTitle = '//div[@id="centerCol"]/div/div/h1/span[@id="ebooksProductTitle"]/text()';
-        $entriesTitle = $xpathItem->query($queryTitle);
-
-	//La variabile in cui mantengo il titolo trovato, sarebbe necessario estrarre il primo elemento da entriesTitle, ma al momento non ho capito come fare
-	//quindi ho dovuto fare un for ignorante
-	$t;
-	foreach ($entriesTitle as $entryTitle)
+        // Per ogni link corrispondente ad un ebook
+        foreach ($links as $link)
         {
-		$t = $entryTitle->nodeValue;
+                // Bisogna estrarre i parametri dell'ebook nella sua pagina
+                $page = file_get_contents($link);
+                $dom = new DOMDocument();
+                $dom->loadHTML($page);
+                $xpath = new DOMXPath($dom);
+
+                // La query per ottenere il titolo
+                $query = '//div[@id="centerCol"]/div/div/h1/span[@id="ebooksProductTitle"]/text()';
+                $entries = $xpath->query($query);
+
+                // La variabile in cui mantengo il titolo trovato
+                // sarebbe necessario estrarre il primo elemento da entriesTitle
+                // ma al momento non ho capito come fare
+                // quindi ho dovuto fare un for ignorante
+                $t = $entries[0]->nodeValue;
+                // foreach ($entries as $entryTitle)
+                // {
+                //         $t = $entryTitle->nodeValue;
+                // }
+
+                // la query per ottenere l'autore
+                $query = '//div[@id="centerCol"]/div[@id="booksTitle"]/div[@id="bylineInfo"]/span/span[@class="a-declarative"]/a[1]/text()';
+                $entries = $xpath->query($query);
+
+                // Stessa storia per gli altri parametri; autore, prezzo, ecc.
+                $a = $entries[0]->nodeValue;
+                // foreach ($entries as $entryAutore)
+                // {
+                //         $a = $entryAutore->nodeValue;
+                // }
+
+                // la query per ottenere il prezzo
+                $query = '//table[@class="a-lineitem a-spacing-micro"]//tr[@class="kindle-price"]/td[2]';
+                $entries = $xpath->query($query);
+
+                $p = $entries[0]->nodeValue;
+                // foreach ($entries as $entryPrezzo)
+                // {
+                //         $p = $entryPrezzo->firstChild->nodeValue;
+                // }
+                $p = \floatval($p);
+
+                // la query per ottenere l'editore
+                $query = '//div[@id="detail_bullets_id"]/table//tr/td[@class="bucket"]/div/ul/li[4]/text()';
+                $entries = $xpath->query($query);
+
+                $e = $entries[0]->nodeValue;
+                // foreach ($entries as $entryEditore)
+                // {
+                //         $e = $entryEditore->nodeValue;
+                // }
+
+                // la query per ottenere l'immagine di copertina
+                $query = '//div[@id="leftCol"]/div[1]/div/div[2]/div/div/div/div/img/attribute::src';
+                $entries = $xpath->query($query);
+
+                $i = $entries[0]->nodeValue;
+                // foreach ($entries as $entryImg)
+                // {
+                //         $i = $entryImg->nodeValue;
+                // }
+
+                // Creo un oggetto di tipo Book con i parametri trovati
+                $b = new Book($t, $a, $p, $i, $link, $e);
+
+                //Inserisco l'oggetto in un array
+                array_push($booksFound, $b);
         }
-
-
-	//Stessa storia per gli altri parametri; autore, prezzo, ecc.
-        $a;
-
-        $queryAutore = '//div[@id="centerCol"]/div[@id="booksTitle"]/div[@id="bylineInfo"]/span/span[@class="a-declarative"]/a[1]/text()';
-
-        $entriesAutore = $xpathItem->query($queryAutore);
-
-        foreach ($entriesAutore as $entryAutore)
-        {
-		$a = $entryAutore->nodeValue;
-        }
-
-	$p;
-
-	$queryPrezzo = '//table[@class="a-lineitem a-spacing-micro"]//tr[@class="kindle-price"]/td[2]';
-
-        $entriesPrezzo = $xpathItem->query($queryPrezzo);
-
-        foreach ($entriesPrezzo as $entryPrezzo)
-        {
-                $p = $entryPrezzo->firstChild->nodeValue;
-        }
-
-        $e;
-
-        $queryEditore = '//div[@id="detail_bullets_id"]/table//tr/td[@class="bucket"]/div/ul/li[4]/text()';
-
-        $entriesEditore = $xpathItem->query($queryEditore);
-
-        foreach ($entriesEditore as $entryEditore)
-        {
-                $e = $entryEditore->nodeValue;
-        }
-
-        $i;
-
-	$queryImg = '//div[@id="leftCol"]/div[1]/div/div[2]/div/div/div/div/img/attribute::src';
-
-        $entriesImg = $xpathItem->query($queryImg);
-
-        foreach ($entriesImg as $entryImg)
-        {
-                $i = $entryImg->nodeValue;
-        }
-
-	//Creo un oggetto di tipo Book con i parametri trovati
-	$b = new Book($t, $a, $p, $i, $link, $e);
-
-	//Inserisco l'oggetto in un array
-        array_push($booksFound, $b);
-}
 
         return $booksFound;
-
-//Controllo i parametri di ogni libro
-//foreach ($books as $book)
-//        $book->call();
 }
-?>
+
+// TODO remove all the code after (and including) this line
+$books = getAmazonBooks('il signore degli anelli');
+
+// Controllo i parametri di ogni libro
+foreach ($books as $book)
+        print $book;
--- wrappers/kobo.php
+++ wrappers/kobo.php
@@ -1,158 +1,132 @@
 <?php
 
-class Book
-{
-        private $titolo;
-        private $autore;
-        private $prezzo;
-        private $img;
-        private $link;
-	private $editore;
+namespace wrappers;
 
-        public function __construct($t, $a, $p, $i, $l, $e)
-        {
-        $this->titolo = $t;
-	$this->autore = $a;
-	$this->prezzo = $p;
-	$this->img = $i;
-	$this->link = $l;
-	$this->editore = $e;
-	}
+require '..\model\Book.php';
 
-        public function call()
-        {
-	echo "titolo: " . ($this->titolo) . ",<br>autore: " . ($this->autore) . ",<br>prezzo: " . ($this->prezzo) .
-                ",<br>immmagine: " . ($this->img) . ",<br>link: " . ($this->link). ",<br>editore: " . ($this->editore) . "<hr>";
-        }
-
-}
+use \model\Book;
+use \DOMDocument;
+use \DOMXPath;
 
 function getKoboBooks($keyword)
 {
+	$koboURL = 'https://www.kobo.com/it/it/search?Query=';
+	$keyword = str_replace(" ", "+", $keyword);
+	$urlSearch = $koboURL . $keyword . '&fclanguages=it';
 
-$urlSearch = str_replace(" ", "+", "https://www.kobo.com/it/it/search?Query=" . $keyword . "&fclanguages=it");
+	$page = file_get_contents($urlSearch);
 
-$page = file_get_contents($urlSearch);
+	$dom = new DOMDocument;
+	$dom->loadHTML($page);
+	$xpath = new DOMXPath($dom);
 
-$DOM = new DOMDocument;
+	$query = '//div[@class="item-info"]/p[@class="title product-field"]/a/attribute::href';
+	$entries = $xpath->query($query);
 
-$DOM->loadHTML($page);
+	//echo "Trovati: " . $entriesLink->length . "<hr>";
 
-$xpath = new DOMXPath($DOM);
+	$links = array();
 
-$queryLink = '//div[@class="item-info"]/p[@class="title product-field"]/a/attribute::href';
+	foreach ($entries as $entryLink)
+	{
+		$l =  $entryLink->firstChild->nodeValue;
+		array_push($links, $l);
+	}
 
-$entriesLink = $xpath->query($queryLink);
+	$books = searchBooks($links);
 
-//echo "Trovati: " . $entriesLink->length . "<hr>";
+	return $books;
 
-$links = array();
-
-foreach ($entriesLink as $entryLink)
-{
-        $l =  $entryLink->firstChild->nodeValue;
-        array_push($links, $l);
-}
-
-$books = searchBooks($links);
-
-return $books;
-
-//foreach ($books as $book)
-//	$book->call();
+	//foreach ($books as $book)
+	//	$book->call();
 }
 
 function searchBooks($links)
 {
 
-$booksFound = array();
+	$booksFound = array();
 
-foreach ($links as $link)
-{
-	$pageItem = file_get_contents($link);
-
-	$DOMItem = new DOMDocument();
-
-	$DOMItem->loadHTML($pageItem);
-
-	$xpathItem = new DOMXPath($DOMItem);
-
-	$queryTitle = '//div[@class="item-info"]/h1/span/text()';
-
-	$entriesTitle = $xpathItem->query($queryTitle);
-
-	$t;
-
-	foreach ($entriesTitle as $entryTitle)
+	foreach ($links as $link)
 	{
-		$t = $entryTitle->nodeValue;
+		$page = file_get_contents($link);
+
+		$dom = new DOMDocument();
+		$dom->loadHTML($page);
+		$xpath = new DOMXPath($dom);
+
+		$query = '//div[@class="item-info"]/h1/span/text()';
+		$entries = $xpath->query($query);
+
+		//
+		$t = $entries[0]->nodeValue;
+		// foreach ($entries as $entryTitle)
+		// {
+		// 	$t = $entryTitle->nodeValue;
+		// }
+
+		$query = '//div[@class="item-info"]/div/h2/span/span[@class="visible-contributors"]/a[1]/text()';
+		$entries = $xpath->query($query);
+
+		//
+		$a = $entries[0]->nodeValue;
+		foreach ($entriesAutore as $entryAutore)
+		{
+			$a = $entryAutore->nodeValue;
+		}
+
+		$query = '//div[@class="primary-right-container"]/div/div/div/div/div/span/text()';
+		$entries = $xpath->query($query);
+
+		//
+		$p = $entries[0]->nodeValue;
+		foreach ($entries as $entryPrezzo)
+		{
+			$p = $entryPrezzo->nodeValue;
+		}
+		$p = \floatval($p);
+
+		/*
+		// query for number of pages
+		$query = '//section[@class="visible"/div/div/div/div[@class="stat-desc"]/strong/text()';
+		$query = '//div[@class="support title-widget"]/div/section';
+		$entries = $xpath->query($query);
+
+		echo "numpag: " . $entries->length . "<br />";
+
+		echo "numPages: $entries[0]->nodeValue<br />"
+		// foreach ($entries as $entryNumPagine)
+		// {
+		// 	echo "numPagine: " . $entryNumPagine->nodeValue . "<br />";
+		// }
+		*/
+
+		$query = '//div[@class="BookItemDetailSecondaryMetadataWidget"]/div/div/div/ul/li/a[@class="description-anchor"]/span/text()';
+		$entries = $xpath->query($query);
+
+		$e = $entries[0]->nodeValue;
+		// foreach ($entriesEditore as $entryEditore)
+		// {
+		// 	$e = $entryEditore->nodeValue;
+		// }
+
+		$query = '//div[@class="primary-left-container"]/div/div/div/div/div/img/attribute::src';
+		$entries = $xpath->query($query);
+
+		//
+		$i = $entries[0]->nodeValue;
+		// foreach ($entriesImg as $entryImg)
+		// {
+		// 	$i = "https:" . $entryImg->firstChild->nodeValue;
+		// }
+
+		$b = new Book($t, $a, $p, $i, $link, $e);
+		array_push($booksFound, $b);
 	}
-
-	$a;
-
-        $queryAutore = '//div[@class="item-info"]/div/h2/span/span[@class="visible-contributors"]/a[1]/text()';
-
-        $entriesAutore = $xpathItem->query($queryAutore);
-
-        foreach ($entriesAutore as $entryAutore)
-        {
-		$a = $entryAutore->nodeValue;
-	}
-
-	$p;
-
-        $queryPrezzo = '//div[@class="primary-right-container"]/div/div/div/div/div/span/text()';
-
-        $entriesPrezzo = $xpathItem->query($queryPrezzo);
-
-        foreach ($entriesPrezzo as $entryPrezzo)
-	{
-		$p = $entryPrezzo->nodeValue;
-	}
-
-/*	$queryNumPagine = '//section[@class="visible"/div/div/div/div[@class="stat-desc"]/strong/text()';
-
-	$queryNumPagine = '//div[@class="support title-widget"]/div/section';
-
-        $entriesNumPagine = $xpathItem->query($queryNumPagine);
-
-	echo "numpag: " . $entriesNumPagine->length . "<br>";
-
-        foreach ($entriesNumPagine as $entryNumPagine)
-                echo "numPagine: " . $entryNumPagine->nodeValue . "<br>";
-*/
-	$e;
-
-        $queryEditore = '//div[@class="BookItemDetailSecondaryMetadataWidget"]/div/div/div/ul/li/a[@class="description-anchor"]/span/text()';
-
-        $entriesEditore = $xpathItem->query($queryEditore);
-
-        foreach ($entriesEditore as $entryEditore)
-        {
-		$e = $entryEditore->nodeValue;
-	}
-
-	$i;
-
-	$queryImg = '//div[@class="primary-left-container"]/div/div/div/div/div/img/attribute::src';
-
-        $entriesImg = $xpathItem->query($queryImg);
-
-        foreach ($entriesImg as $entryImg)
-        {
-		$i = "https:" . $entryImg->firstChild->nodeValue;
-	}
-
-	$b = new Book($t, $a, $p, $i, $link, $e);
-
-	array_push($booksFound, $b);
-}
 
 	return $booksFound;
-
-//foreach ($books as $book)
-//	$book->call();
 }
 
-//searchBooks("harry potter");
-?>
+//$books = searchBooks('harry potter');
+
+//foreach ($books as $book)
+//	print $book;
