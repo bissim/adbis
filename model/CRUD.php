605664420f23afd801a6b7d0c7edf00e825cbe4e
Simone B <s.bisogno10@studenti.unisa.it>
2018-09-09 01:31:09 +0200

    Add CRUD interface for DAO classes

---
+++ model/CRUD.php
@@ -0,0 +1,18 @@
+<?php
+/**
+     * Created by PhpStorm.
+     * User: bisim
+     * Date: 09/09/2018
+     * Time: 01:22
+     */
+
+namespace model;
+
+
+interface CRUD
+{
+    public function create(object $entity): object;
+    public function retrieve(object $entity): object;
+    public function update(object $entity): object;
+    public function delete(object $entity): object;
+}
