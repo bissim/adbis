0920ad6699ccc1555801aa642a732fa86ca8a088
AddeusExMachina <antaddnf@gmail.com>
2018-09-09 15:52:51 +0200

    db commit

--- db/adbis_create.sql
+++ db/adbis_create.sql
@@ -23,14 +23,30 @@ use adbis_db;
 drop table if exists book;
 
 create table if not exists book (
-  id integer(4) auto_increment unique
+  id integer(4) auto_increment unique,
+  title VARCHAR(30) not null,
+  author VARCHAR(30) not null,
+  price FLOAT not null,
+  image VARCHAR(50),
+  link VARCHAR(50) not null,
+  editor VARCHAR(30),
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 # review table
 drop table if exists review;
 
 create table if not exists review(
-  id integer(4) auto_increment unique
+  id integer(4) auto_increment unique,
+  title VARCHAR(30) not null,
+  author VARCHAR(30) not null,
+  plot text not null,
+  text text not null,
+  average FLOAT,
+  style FLOAT,
+  content FLOAT,
+  pleasentness FLOAT,
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 #
