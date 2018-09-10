e8978ca480b15c5761c74e5da278770f49ea7707
Simone B <s.bisogno10@studenti.unisa.it>
2018-09-10 19:01:04 +0200

    Test DB and Book persistence

---
+++ test/DBManagerTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bisim
+ * Date: 10/09/2018
+ * Time: 10:11
+ */
+
+namespace test;
+
+require '../controller/DBManager.php';
+
+use controller\DBManager;
+
+$dbMan = new DBManager('localhost', 'adbis', '123456', 'adbis_db', '3306');
+echo "$dbMan<br />";
+try {
+    $dbMan->connect();
+    echo "$dbMan<br />";
+    echo $dbMan->isConnected() ? 'FUCK YEA' : 'NO FUCK NO';
+}
+catch (\Exception $e)
+{
+    error_log("An exception occurred: $e->getMessage().");
+}
