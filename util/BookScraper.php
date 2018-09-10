3e30eaabd3596ba4b0d817b3d28e773675bdff18
AddeusExMachina <antaddnf@gmail.com>
2018-09-10 18:12:11 +0200

    getWebPage function added

--- util/BookScraper.php
+++ util/BookScraper.php
@@ -35,54 +35,34 @@
                 $urlSearch .= $urlSuffix;
             }
 
-            // create DOMXPath object for XPath queries
-            $xpath = $this->createDOMXPath($urlSearch);
-
-            // query results page
-            $entries = $xpath->query($this->queries['links']);
-//        print_r($entries); // TODO remove
-
-            // print_r($entries[0]->firstChild->nodeValue . "<br>");
-
-            // put retrieved links into array
-            $links = array();
-//        echo '<br /><br />'; // TODO remove
-            foreach ($entries as $entryLink)
-//        for ($i = 0; $i < 1; $i++) // TODO test ebook retrieval
-            {
-                $l = $entryLink->firstChild->nodeValue;
-//            $l = $entries[$i]->firstChild->nodeValue;
-        //    print_r($domain . $l); // TODO remove
-        //    echo '<br /><br />';
-                array_push($links, $domain . $l);
-            }
-
-            // foreach ($links as $link)
-            //     print_r($link . "<br>");
-
-            // retrieve books from links
-            // as array of Book objects
-            $books = $this->searchBooks($links);
+            $books = $this->searchBooks($urlSearch);
 
             return $books;
         }
 
+        private function getWebPage(string $url) {
+            $options = array(
+                CURLOPT_RETURNTRANSFER => true,   // return web page
+                CURLOPT_HEADER         => false,  // don't return headers
+                CURLOPT_FOLLOWLOCATION => true,   // follow redirects
+                CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
+                CURLOPT_ENCODING       => '',     // handle compressed
+                CURLOPT_USERAGENT      => 'Mozilla/5.0 (Android 7.0; Mobile; rv:57.0) Gecko/57.0 Firefox/57.0', // name of client
+                CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
+                CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
+                CURLOPT_TIMEOUT        => 120,    // time-out on response
+            );
+        
+            $ch = curl_init($url);
+            curl_setopt_array($ch, $options);
+            $content  = curl_exec($ch);    
+            curl_close($ch);    
+            return $content;
+        }
+
         private function createDOMXPath(string $url): DOMXPath
         {
-//        echo "URL to scrape: $url<br />"; // TODO remove
-
-            // load page
-            $curl = curl_init($url);
-            curl_setopt($curl, CURLOPT_URL, $url);
-            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($curl, CURLOPT_USERAGENT, 'Mozilla/5.0 (Android 7.0; Mobile; rv:57.0) Gecko/57.0 Firefox/57.0');
-            $page = curl_exec($curl);
-            if (curl_errno($curl)) // check for execution errors
-            {
-                echo 'Scraper error ' . curl_errno($curl) . ": " . curl_error($curl);
-                exit;
-            }
-            curl_close($curl);
+            $page = $this->getWebPage($url);
 
             // create DOM
             $dom = new DOMDocument;
@@ -96,26 +76,30 @@
             return $xpath;
         }
 
-        private function searchBooks(array $links): array
+        private function searchBooks(string $queryUrl)
         {
+            $xpath = $this->createDOMXPath($queryUrl);
+
             $booksFound = array();
 
-//        $link = $links[0]; // TODO remove
-            // explore every link
-            foreach ($links as $link)
-            {
-                $attributes = $this->extractAttributes($link);
+            $entriesTitle = $xpath->query($this->queries['title']);
+            $entriesAuthor = $xpath->query($this->queries['author']);
+            $entriesPrice = $xpath->query($this->queries['price']);
+            $entriesImage = $xpath->query($this->queries['image']);
+            $entriesLink = $xpath->query($this->queries['link']);
+            
+            $length = $entriesAuthor->length;
 
-                // create a Book object and put it in array
+            for ($i=0; $i<$length; $i++)
+            {
                 $book = new Book(
-                    $attributes['title'],
-                    $attributes['author'],
-                    $attributes['price'],
-                    $attributes['image'],
-                    $attributes['shortLink'],
-                    $attributes['editor']
+                    $this->checkEmpty($entriesTitle[$i]->nodeValue),
+                    $this->checkEmpty($entriesAuthor[$i]->nodeValue),
+                    $this->checkFloat($entriesPrice[$i]->nodeValue),
+                    $this->checkEmpty($entriesImage[$i]->nodeValue),
+                    $this->checkEmpty($entriesLink[$i]->nodeValue)
                 );
-                array_push($booksFound, $book);
+                array_push($booksFound,$book);
             }
 
             return $booksFound;
@@ -126,51 +110,10 @@
             return empty($value) ? '' : $value;
         }
 
-        private function extractAttributes(string $link): array
+        private function checkFloat($value)
         {
-//        echo "<br />execute $query";
-            $xpath = $this->createDOMXPath($link);
-
-            $attributes = array();
-
-            $entriesTitle = $xpath->query($this->queries['title']);
-            $title = $this->checkEmpty($entriesTitle[0]->nodeValue);
-
-            $entriesAuthor = $xpath->query($this->queries['author']);
-            $author = $this->checkEmpty($entriesAuthor[0]->nodeValue);
-
-            $entriesPrice = $xpath->query($this->queries['price']);
-            $stringPrice = $entriesPrice[0]->nodeValue;
-            $stringPrice = \substr($stringPrice,5);
-            $stringPrice = str_replace(',', '.', $stringPrice);
-            $price = empty($stringPrice) ? 0.0 : (float) \floatval($stringPrice);
-
-            $entriesEditor = $xpath->query($this->queries['editor']);
-            $editor = $this->checkEmpty($entriesEditor[0]->nodeValue);
-
-            $entriesImage = $xpath->query($this->queries['image']);
-            $image = $this->checkEmpty($entriesImage[0]->nodeValue);
-
-            $entriesLink = $xpath->query($this->queries['link']);
-            $shortLink = $this->checkEmpty($entriesLink[0]->nodeValue);
-
-            $attributes = array(
-                "title" => $title,
-                "author" => $author,
-                "price" => $price,
-                "editor" => $editor,
-                "image" => $image,
-                "shortLink" => $shortLink
-            );
-
-        //     if (empty($attribute))
-        //     {
-        //    echo '<br />empty attribute!<br /><hr />';
-        //         return '';
-        //     }
-//            echo "<br />Attribute: ";
-//            print_r($attribute); // TODO remove
-//            echo "<hr />";
-            return $attributes;
+            $value = preg_replace('/[^0-9,.]/', '', $value);
+            $value = str_replace(',', '.', $value);
+            return empty($value) ? 0.0 : (float) \floatval($value);
         }
-    }
+}
