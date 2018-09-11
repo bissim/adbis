f1932646af0d734d6f0b01f777f8c4e01674f8e1
AddeusExMachina <antaddnf@gmail.com>
2018-09-11 14:39:42 +0200

    curlopt_useragent changed

--- util/BookScraper.php
+++ util/BookScraper.php
@@ -47,7 +47,7 @@
                 CURLOPT_FOLLOWLOCATION => true,   // follow redirects
                 CURLOPT_MAXREDIRS      => 10,     // stop after 10 redirects
                 CURLOPT_ENCODING       => '',     // handle compressed
-                CURLOPT_USERAGENT      => 'Mozilla/5.0 (Android 7.0; Mobile; rv:57.0) Gecko/57.0 Firefox/57.0', // name of client
+                CURLOPT_USERAGENT      => 'adbis', // name of client
                 CURLOPT_AUTOREFERER    => true,   // set referrer on redirect
                 CURLOPT_CONNECTTIMEOUT => 120,    // time-out on connect
                 CURLOPT_TIMEOUT        => 120,    // time-out on response
@@ -55,15 +55,15 @@
         
             $ch = curl_init($url);
             curl_setopt_array($ch, $options);
-            $content  = curl_exec($ch);    
-            curl_close($ch);    
+            $content  = curl_exec($ch);
+            curl_close($ch);
             return $content;
         }
 
         private function createDOMXPath(string $url): DOMXPath
         {
             $page = $this->getWebPage($url);
-
+            // $page = file_get_contents($url);
             // create DOM
             $dom = new DOMDocument;
             libxml_use_internal_errors(true);
