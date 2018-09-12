5da5e832366e56e352e43cecfc6e0543777b0d6b
AddeusExMachina <antaddnf@gmail.com>
2018-09-12 09:07:28 +0200

    data exchange with json

---
+++ test/Mediator.php
@@ -0,0 +1,25 @@
+<?php
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once '../model/Book.php';
+require '../controller/BookDAO.php';
+
+require_once '../model/Review.php';
+require '../controller/ReviewDAO.php';
+
+use model\Book;
+use controller\BookDAO;
+
+use model\Review;
+use controller\ReviewDAO;
+
+$bookDao = new BookDAO;
+$bookReturned = $bookDao->retrieveByAuthor('aut');
+echo json_encode($bookReturned);
+
+$reviewDao = new ReviewDAO;
+$reviewReturned = $reviewDao->retrieveByAuthor('aut');
+echo json_encode($reviewReturned);
+
+
+?>
