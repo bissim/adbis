5df63ed198210b21947ad72f1aaac211a0eb2fc4
nyds <simego@hotmail.it>
2018-09-14 12:07:58 +0200

    Fix wrong attribute name in dummy populate.

--- db/adbis_dummy_populate.sql
+++ db/adbis_dummy_populate.sql
@@ -3,6 +3,6 @@ insert into book (title, author, price, image, link)
           ('Title 2', 'Author 2', 2.0, 'Image 2', 'Link 2'),
           ('Title 3', 'Author 3', 3.0, 'Image 3', 'Link 3');
 
-insert into review (title, author, plot, txt, average, style, content, pleasentness)
+insert into review (title, author, plot, txt, average, style, content, pleasantness)
   values ('Title 1', 'Author 1', "A certain plot 1", "A certain text 1", 1.0, 3.0, 5.0, 7.0),
           ('Title 2', 'Author 2', "A certain plot 2", "A certain text 2", 2.0, 4.0, 6.0, 8.0);
