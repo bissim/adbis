c0687f15999aac05f5d0a53683fa88ed336a5baa
AddeusExMachina <antaddnf@gmail.com>
2018-09-14 11:32:01 +0200

    realized js file called by index.php

--- view/index.php
+++ view/index.php
@@ -14,18 +14,48 @@ include '../Autoloader.php';
 
 <html>
 
-    <head>
-        <meta charset="utf-8" />
-        <title>Prova</title>
-    </head>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<meta charset="utf-8" />
+<title>Prova</title>
 
-    <body>
+<body>
 
-        <form action="main.php" method="post">
-            Digita il nome di un autore o di un libro <input type="text" name="author"><br>
-            <input type="submit">
-        </form>
+<!-- Qui saranno posizionaati i risultati della ricerca -->
+<p id="demo"></p>
 
-    </body>
+<script type="text/javascript" src="main.js"></script>
+
+<h2>Libri</h2>
+<!-- Il form per la ricerca dei libri in base all'autore -->
+<form name="authorForm">
+    Digita il nome di un autore <input type="text" id="bookAut" name="author"><br>
+    <input id="bookAutBtn" type="button" value="Invia">
+</form>
+
+<!-- Il form per la ricerca dei libri in base al titolo -->
+<form name="titleForm">
+    Digita il titolo di un libro <input type="text" id="bookTit" name="title"><br>
+    <input id="bookTitBtn" type="button" value="Invia">
+</form>
+
+<hr />
+
+<h2>Recensioni</h2>
+<!-- Il form per la ricerca delle recensioni in base all'autore -->
+<form name="authorForm">
+        Digita il nome di un autore <input type="text" id="reviewAut" name="author"><br>
+        <input id="reviewAutBtn" type="button" value="Invia">
+    </form>
+    
+    <!-- Il form per la ricerca delle recensioni in base al titolo -->
+    <form name="titleForm">
+        Digita il titolo di un libro <input type="text" id="reviewTit" name="title"><br>
+        <input id="reviewTitBtn" type="button" value="Invia">
+    </form>
+
+</body>
 
 </html>
+
+
---
+++ view/main.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    // Si distingue il caso di ricerca per autore e ricerca per titolo
+    $('input[type=button]').click(function(){
+        var id = $(this).attr("id");
+        var keyword, search, table;
+        switch (id) {
+            case 'bookAutBtn': keyword = $('input[id=bookAut]').val();
+                            search = 'author';
+                            table = 'book';
+            break;
+            case 'bookTitBtn': keyword = $('input[id=bookTit]').val();
+                            search = 'title';
+                            table = 'book';
+            break;
+            case 'reviewAutBtn': keyword = $('input[id=reviewAut]').val();
+                            search = 'author';
+                            table = 'review';
+            break;
+            case 'reviewTitBtn': keyword = $('input[id=reviewTit]').val();
+                            search = 'title';
+                            table = 'review';
+        }
+        // Il json che si invia alla pagina php consiste nella parola chiave per la ricerca
+        // e il tipo di ricerca (per autore o titolo)
+        obj = { "keyword":keyword, "search":search, "table":table };
+        dbParam = JSON.stringify(obj);
+        // Si effettua la chiamata AJAX
+        $.ajax({
+            type : "GET",
+            url : "../controller/Mediator.php?x=" + dbParam,
+            dataType : "json",
+            // Si stampano i risultati ottenuti
+            success : function (res) {
+                var txt = "";
+                if (table == "book")
+                {
+                    $.each( res, function( i, value ) {
+                    txt += "Titolo: " + value.title + "<br>" + 
+                            "Autore: " + value.author + "<br>" +
+                            "Prezzo: " + value.price + "<br>" + 
+                            "Immagine: " + value.image + "<br>" +
+                            "Link: " + value.link + "<hr>";
+                });
+            }
+            else {
+                $.each( res, function( i, value ) {
+                    txt += "Titolo: " + value.title + "<br>" + 
+                            "Autore: " + value.author + "<br>" +
+                            "Trama: " + value.plot + "<br>" + 
+                            "Testo: " + value.text + "<br>" +
+                            "Media: " + value.avg + "<br>" +
+                            "Stile: " + value.style + "<br>" +
+                            "Contenuto: " + value.content + "<br>" +
+                            "Piacevolezza: " + value.pleasantness + "<hr>";
+                });                
+            }
+            $("#demo").html(txt);
+            },
+            error : function (richiesta,stato,errori) {
+                alert("E' avvenuto un errore. stato della chiamata: "+stato);
+            }
+        });
+    });
+});
