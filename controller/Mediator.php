36eb781d82046d931f02013a8228b4b026312db0
nyds <simego@hotmail.it>
2018-09-21 19:08:54 +0200

    Update Mediator

--- controller/Mediator.php
+++ controller/Mediator.php
@@ -4,12 +4,16 @@
     require_once './model/Book.php';
     require_once './model/Review.php';
 
-    require './util/ErrorHandler.php';
+    require_once './util/ErrorHandler.php';
 
     require './controller/WrapperManager.php';
     require './controller/DAOManager.php';
 
-    class Mediator {
+    use \controller\DAOManager;
+    use \controller\WrapperManager;
+
+    class Mediator
+    {
 
         public function __construct()
         {}
@@ -24,40 +28,35 @@
          */
         public function retrieve(string $table, string $search, string $keyword): string
         {
-            switch ($table)
+            try
             {
-                case 'book':
-                    try
+                switch ($table)
+                {
+                    case 'book':
                     {
                         $result = $this->jsonEncodeBooks($search, $keyword);
+                        break;
                     }
-                    catch (\Throwable $th)
-                    {
-                        throw $th;
-                    }
-                    break;
-                case 'review':
-                    try
+                    case 'review':
                     {
                         $result = $this->jsonEncodeReviews($search, $keyword);
+                        break;
                     }
-                    catch (\Throwable $th)
-                    {
-                        throw $th;
-                    }
-                    break;
                     case 'join':
-                    try
                     {
                         $result = $this->jsonEncodeBoth($search, $keyword);
+                        break;
                     }
-                    catch (\Throwable $th)
+                    default:
                     {
-                        throw $th;
+                        throw new \Exception("unknown table $table.");
+                        break;
                     }
-                    break;                default:
-                    throw new \Exception("unknown table $table.");
-                    break;
+                }
+            }
+            catch (\Throwable $th)
+            {
+                throw $th;
             }
 
             return $result;
@@ -72,11 +71,11 @@
          */
         private function jsonEncodeBooks(string $search, string $keyword): string
         {
-            $daoMng = new DAOManager();
+            $daoMng = new DAOManager;
             $books = $daoMng->getBooks($search, $keyword);
             if (empty($books))
             {
-                $wrapperMng = new WrapperManager();
+                $wrapperMng = new WrapperManager;
                 $daoMng->addBooks($wrapperMng->getBooks($keyword));
                 $books = $daoMng->getBooks($search, $keyword);
             }
@@ -93,11 +92,11 @@
          */
         private function jsonEncodeReviews(string $search, string $keyword): string
         {
-            $daoMng = new DAOManager();
+            $daoMng = new DAOManager;
             $reviews = $daoMng->getReviews($search, $keyword);
             if (empty($reviews))
             {
-                $wrapperMng = new WrapperManager();
+                $wrapperMng = new WrapperManager;
                 $daoMng->addReviews($wrapperMng->getReviews($keyword));
                 $reviews = $daoMng->getReviews($search, $keyword);
             }
@@ -114,30 +113,34 @@
          */
         private function jsonEncodeBoth(string $search, string $keyword): string
         {
-            $daoMng = new DAOManager();
+            $daoMng = new DAOManager;
+            // check in db first
             $books = $daoMng->getBooks($search, $keyword);
             $reviews = $daoMng->getReviews($search, $keyword);
 
             if (empty($books))
             {
-                $wrapperMng = new WrapperManager();
+                // scrape books from sources
+                $wrapperMng = new WrapperManager;
                 $daoMng->addBooks($wrapperMng->getBooks($keyword));
                 $books = $daoMng->getBooks($search, $keyword);
             }
 
             if (empty($reviews))
             {
-                $wrapperMng = new WrapperManager();
+                // scrape reviews from source
+                $wrapperMng = new WrapperManager;
                 $daoMng->addReviews($wrapperMng->getReviews($keyword));
                 $reviews = $daoMng->getReviews($search, $keyword);
             }
 
-            $booksFound = array();
+            $booksFound['books'] = array();
             $booksFound['reviews'] = array();
 
             foreach ($books as $book) // TODO Y U ARRAY
             {
-                array_push($booksFound, $book);
+                array_push($booksFound['books'], $book);
+
                 foreach ($reviews as $review)
                 {
                     if ($book['title'] === $review['title'])
