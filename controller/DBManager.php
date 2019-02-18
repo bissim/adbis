03ce50e05de40d527acd7d8905b47cb7b71f7c78
Simone B <s.bisogno10@studenti.unisa.it>
2019-02-18 22:14:00 +0100

    Minor linting to DBManager class

--- controller/DBManager.php
+++ controller/DBManager.php
@@ -23,7 +23,10 @@
         public function __construct()
         {
             // retrieve configurations from file
-            $configs = parse_ini_file('./db/dbconfig.ini', true);
+            $configs = parse_ini_file(
+                './db/dbconfig.ini',
+                true
+            );
 
             // set DB connection parameters
             $this->serverName = $configs['connection']['server'];
@@ -33,61 +36,98 @@
             $this->port = $configs['connection']['port'];
         }
 
-        private function connect() {
-            try {
-                $this->conn = new PDO("mysql:host=$this->serverName;dbname=$this->dbName",
-                                $this->username, $this->password);
+        private function connect()
+        {
+            try
+            {
+                $this->conn = new \PDO(
+                    "mysql:host=$this->serverName;dbname=$this->dbName",
+                    $this->username,
+                    $this->password
+                );
+
                 // set the PDO error mode to exception
-                $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                }
-            catch(\PDOException $e)
-                {
+                $this->conn->setAttribute(
+                    \PDO::ATTR_ERRMODE,
+                    \PDO::ERRMODE_EXCEPTION
+                );
+            }
+            catch (\PDOException $e)
+            {
                 echo $e->getMessage();
-                }
+            }
         }
 
-        private function disconnect() {
+        private function disconnect()
+        {
             $this->conn = null;
         }
         
-        public function getAllBooks(): array {
+        public function getAllBooks(): array
+        {
             $this->connect();
             $sql = "SELECT * FROM book";
             $result = $this->conn->query($sql);
 
             $books = array();
-            if ($result->rowCount() > 0) {
-                while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    array_push($books,
-                                new Book($row["title"],$row["author"],$row["price"],$row["img"],$row["link"],$row["source"])
+            if ($result->rowCount() > 0)
+            {
+                while ($row = $result->fetch(\PDO::FETCH_ASSOC))
+                {
+                    array_push(
+                        $books,
+                        new Book(
+                            $row["title"],
+                            $row["author"],
+                            $row["price"],
+                            $row["img"],
+                            $row["link"],
+                            $row["source"]
+                        )
                     );
                 }
             }
+
             $this->disconnect();
             return $books;
         }
 
-        public function getNewBooks(): array {
+        public function getNewBooks(): array
+        {
             $this->connect();
             $sql = "SELECT * FROM book WHERE is_recent=1";
             $result = $this->conn->query($sql);
 
             $books = array();
-            if ($result->rowCount() > 0) {
-                while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    array_push($books,
-                                new Book($row["title"],$row["author"],$row["price"],$row["img"],$row["link"],$row["source"])
+            if ($result->rowCount() > 0)
+            {
+                while ($row = $result->fetch(\PDO::FETCH_ASSOC))
+                {
+                    array_push(
+                        $books,
+                        new Book(
+                            $row["title"],
+                            $row["author"],
+                            $row["price"],
+                            $row["img"],
+                            $row["link"],
+                            $row["source"]
+                        )
                     );
                 }
             }
+
             $this->disconnect();
             return $books;
         }
 
-        public function addBooks(array $books) {
+        public function addBooks(array $books)
+        {
             $this->connect();
-            $stmt = $this->conn->prepare("INSERT INTO book (title,author,price,img,link,is_recent,source)
-                                        VALUES (:title,:author,:price,:img,:link,:is_recent,:source)");
+            $stmt = $this->conn->prepare(
+                "INSERT INTO book (title,author,price,img,link,is_recent,source)
+                VALUES (:title,:author,:price,:img,:link,:is_recent,:source)"
+            );
             $stmt->bindParam(':title', $title);
             $stmt->bindParam(':author', $author);
             $stmt->bindParam(':price', $price);
@@ -96,7 +136,8 @@
             $stmt->bindParam(':is_recent', $isRecent);
             $stmt->bindParam(':source', $source);
 
-            foreach($books as $book) {
+            foreach ($books as $book)
+            {
                 $title = $book->getTitle();
                 $author = $book->getAuthor();
                 $price = $book->getPrice();
@@ -104,49 +145,76 @@
                 $link = $book->getLink();
                 $isRecent = $book->isRecent() ? 1 : 0;
                 $source = $book->getSource();
+
                 $stmt->execute();
             }
+
             $this->disconnect();
         }
 
-        public function getAllAudioBooks(): array {
+        public function getAllAudioBooks(): array
+        {
             $this->connect();
             $sql = "SELECT * FROM audioBook";
             $result = $this->conn->query($sql);
 
             $books = array();
-            if ($result->rowCount() > 0) {
-                while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    array_push($books,
-                                new AudioBook($row["title"],$row["author"],$row["voice"],$row["img"],$row["link"])
+            if ($result->rowCount() > 0)
+            {
+                while ($row = $result->fetch(\PDO::FETCH_ASSOC))
+                {
+                    array_push(
+                        $books,
+                        new AudioBook(
+                            $row["title"],
+                            $row["author"],
+                            $row["voice"],
+                            $row["img"],
+                            $row["link"]
+                        )
                     );
                 }
             }
+
             $this->disconnect();
             return $books;
         }
 
-        public function getNewAudioBooks(): array {
+        public function getNewAudioBooks(): array
+        {
             $this->connect();
             $sql = "SELECT * FROM audioBook WHERE is_recent=1";
             $result = $this->conn->query($sql);
 
             $books = array();
-            if ($result->rowCount() > 0) {
-                while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    array_push($books,
-                                new AudioBook($row["title"],$row["author"],$row["voice"],$row["img"],$row["link"])
+            if ($result->rowCount() > 0)
+            {
+                while ($row = $result->fetch(\PDO::FETCH_ASSOC))
+                {
+                    array_push(
+                        $books,
+                        new AudioBook(
+                            $row["title"],
+                            $row["author"],
+                            $row["voice"],
+                            $row["img"],
+                            $row["link"]
+                        )
                     );
                 }
             }
+
             $this->disconnect();
             return $books;
         }
 
-        public function addAudioBooks(array $books) {
+        public function addAudioBooks(array $books)
+        {
             $this->connect();
-            $stmt = $this->conn->prepare("INSERT INTO audioBook (title,author,voice,img,link,is_recent)
-                                        VALUES (:title,:author,:voice,:img,:link,:is_recent)");
+            $stmt = $this->conn->prepare(
+                "INSERT INTO audioBook (title,author,voice,img,link,is_recent)
+                VALUES (:title,:author,:voice,:img,:link,:is_recent)"
+            );
             $stmt->bindParam(':title', $title);
             $stmt->bindParam(':author', $author);
             $stmt->bindParam(':voice', $voice);
@@ -154,59 +222,91 @@
             $stmt->bindParam(':link', $link);
             $stmt->bindParam(':is_recent', $isRecent);
 
-            foreach($books as $book) {
+            foreach ($books as $book)
+            {
                 $title = $book->getTitle();
                 $author = $book->getAuthor();
                 $voice = $book->getVoice();
                 $img = $book->getImg();
                 $link = $book->getLink();
                 $isRecent = $book->isRecent() ? 1 : 0;
+
                 $stmt->execute();
             }
+
             $this->disconnect();
         }
 
-        public function getAllReviews(): array {
+        public function getAllReviews(): array
+        {
             $this->connect();
             $sql = "SELECT * FROM review";
             $result = $this->conn->query($sql);
 
             $books = array();
-            if ($result->rowCount() > 0) {
-                while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    array_push($books,
-                                new Review($row["title"],$row["author"],$row["plot"],$row["txt"],
-                                            $row["average"],$row["style"],$row["content"],$row["pleasantness"])
+            if ($result->rowCount() > 0)
+            {
+                while ($row = $result->fetch(\PDO::FETCH_ASSOC))
+                {
+                    array_push(
+                        $books,
+                        new Review(
+                            $row["title"],
+                            $row["author"],
+                            $row["plot"],
+                            $row["txt"],
+                            $row["average"],
+                            $row["style"],
+                            $row["content"],
+                            $row["pleasantness"]
+                        )
                     );
                 }
             }
+
             $this->disconnect();
             return $books;
         }
 
-        public function getNewReviews(): array {
+        public function getNewReviews(): array
+        {
             $this->connect();
             $sql = "SELECT * FROM review WHERE is_recent=1";
             $result = $this->conn->query($sql);
 
             $books = array();
-            if ($result->rowCount() > 0) {
-                while($row = $result->fetch(PDO::FETCH_ASSOC)) {
-                    array_push($books,
-                                new Review($row["title"],$row["author"],$row["plot"],$row["txt"],
-                                            $row["average"],$row["style"],$row["content"],$row["pleasantness"])
+            if ($result->rowCount() > 0)
+            {
+                while ($row = $result->fetch(PDO::FETCH_ASSOC))
+                {
+                    array_push(
+                        $books,
+                        new Review(
+                            $row["title"],
+                            $row["author"],
+                            $row["plot"],
+                            $row["txt"],
+                            $row["average"],
+                            $row["style"],
+                            $row["content"],
+                            $row["pleasantness"]
+                        )
                     );
                 }
             }
+
             $this->disconnect();
             return $books;
         }
 
-        public function addReviews(array $reviews) {
+        public function addReviews(array $reviews)
+        {
             $this->connect();
             $this->connect();
-            $stmt = $this->conn->prepare("INSERT INTO review (title,author,plot,txt,average,style,content,pleasantness,is_recent)
-                                        VALUES (:title,:author,:plot,:txt,:average,:style,:content,:pleasantness,:is_recent)");
+            $stmt = $this->conn->prepare(
+                "INSERT INTO review (title,author,plot,txt,average,style,content,pleasantness,is_recent)
+                VALUES (:title,:author,:plot,:txt,:average,:style,:content,:pleasantness,:is_recent)"
+            );
             $stmt->bindParam(':title', $title);
             $stmt->bindParam(':author', $author);
             $stmt->bindParam(':plot', $plot);
@@ -217,7 +317,8 @@
             $stmt->bindParam(':pleasantness', $pleasantness);
             $stmt->bindParam(':is_recent', $isRecent);
 
-            foreach($reviews as $review) {
+            foreach ($reviews as $review)
+            {
                 $title = $review->getTitle();
                 $author = $review->getAuthor();
                 $plot = $review->getPlot();
@@ -227,8 +328,10 @@
                 $content = $review->getContent();
                 $pleasantness = $review->getPleasantness();
                 $isRecent = $review->isRecent() ? 1 : 0;
+
                 $stmt->execute();
             }
+
             $this->disconnect();
         }
     }
