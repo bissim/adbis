5a87bb0db7d9eebed822caeecfd98bfae693d0d1
Simone B <s.bisogno10@studenti.unisa.it>
2019-02-23 19:51:57 +0100

    Improve time management in tests

--- test/BasicTest.php
+++ test/BasicTest.php
@@ -3,9 +3,30 @@
 
     class BasicTest
     {
+        private $startTime;
+        private $endTime;
+
         public function microtime_float()
         {
             list($usec, $sec) = explode(" ", microtime());
             return ((float) $usec + (float) $sec);
+        }
+
+        public function start_time()
+        {
+            $this->startTime = $this->microtime_float();
+        }
+
+        public function get_elapsed(): string
+        {
+            $this->endTime = $this->microtime_float();
+            $elapsed = $this->endTime - $this->startTime;
+            $elapsed = number_format(
+                $elapsed,
+                5,
+                ',',
+                '.'
+            );
+            return $elapsed;
         }
     }
