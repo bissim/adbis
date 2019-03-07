37c5883d6b35320cea8fecd2b435d2f946ff491d
Simone B <s.bisogno10@studenti.unisa.it>
2019-03-07 21:01:44 +0100

    Fix retrieval of new audiobooks Make use of unified method for audiobook retrieval

--- controller/WrapperManager.php
+++ controller/WrapperManager.php
@@ -22,49 +22,60 @@
 
         public function __construct()
         {
-            $this->amazonWrapper = new AmazonWrapper();
-            $this->googleWrapper = new GoogleWrapper();
-            $this->koboWrapper = new KoboWrapper();
-            $this->reviewWrapper = new ReviewWrapper();
-            $this->audibleWrapper = new AudibleWrapper();
+            $this->amazonWrapper = new AmazonWrapper;
+            $this->googleWrapper = new GoogleWrapper;
+            $this->koboWrapper = new KoboWrapper;
+            $this->reviewWrapper = new ReviewWrapper;
+            $this->audibleWrapper = new AudibleWrapper;
         }
 
         public function getBooks($keyword): array
         {
-            $books = array_merge($this->amazonWrapper->getBooks($keyword),
-                                $this->googleWrapper->getBooks($keyword),
-                                $this->koboWrapper->getBooks($keyword));
+            $books = array_merge(
+                $this->amazonWrapper->getBooks($keyword),
+                $this->googleWrapper->getBooks($keyword),
+                $this->koboWrapper->getBooks($keyword)
+            );
+
             return $books;
         }
 
         public function getReviews($keyword): array
         {
             $reviews = $this->reviewWrapper->getReviews($keyword);
+
             return $reviews;
         }
 
         public function getAudioBooks($keyword): array
         {
             $books = $this->audibleWrapper->getBooks($keyword);
+
             return $books;
         }
         
         public function getNewBooks(): array
         {
-            $books = array_merge($this->amazonWrapper->getNewBooks(),
-                                $this->koboWrapper->getNewBooks());
+            $books = array_merge(
+                $this->amazonWrapper->getNewBooks(),
+                $this->koboWrapper->getNewBooks()
+            );
+
             return $books;
         }
 
         public function getNewReviews(): array
         {
             $reviews = $this->reviewWrapper->getNewReviews();
+
             return $reviews;
         }
 
         public function getNewAudioBooks(): array
         {
-            $books = $this->audibleWrapper->getNewBooks();
+//            $books = $this->audibleWrapper->getNewBooks();
+            $books = $this->audibleWrapper->getBooks('', true);
+
             return $books;
         }
 
