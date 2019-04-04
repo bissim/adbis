f41b67e1c402ab13f8301b2afc50df9b05d6106e
nyds <simego@hotmail.it>
2019-04-04 18:05:26 +0200

    Update info in about page

--- view/about.php
+++ view/about.php
@@ -45,9 +45,26 @@
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
-          <p>Gli sviluppatori di <strong>AdBis</strong> sono <em>Antonio Addeo</em> e <em>Simone Bisogno</em>, studenti presso il <a href="http://www.di.unisa.it">Dipartimento di Informatica</a> dell'Università degli Studi di Salerno.</p>
-          <p>Questo sito è un progetto applicativo di <em>integrazione dati</em> per il corso di <a href="https://corsi.unisa.it/informatica-magistrale/didattica/insegnamenti?anno=2017&id=507522">Integrazione Dati</a> su Web per l'anno accademico 2017-2018 tenuto dal Prof. <a href="https://rubrica.unisa.it/persone?matricola=001602">Gennato Costagliola</a>.</p>
-          <p>Le fonti utilizzate per questo sito sono lo store online <a href="https://www.amazon.it"><em>Amazon</em></a>, lo store online <a href="https://www.kobo.com/it"><em>Kobo</em></a>, <a href="https://books.google.com/?hl=it"><em>Google Libri</em></a> e <a href="https://www.qlibri.it"><em>QLibri</em></a>; le prime tre fonti sono state interrogate per recuperare informazioni sul sito mentre l'ultima per informazioni sulle recensioni.</p>
+          <p>
+            Gli sviluppatori di <strong>AdBis</strong> sono <em>Antonio Addeo</em>
+            e <em>Simone Bisogno</em>, studenti presso il
+            <a href="http://www.di.unisa.it">Dipartimento di Informatica</a>
+            dell'Università degli Studi di Salerno.
+          </p>
+
+          <p>
+            Questo sito è un progetto applicativo di <em>integrazione dati</em>
+            per il corso di <a href="https://corsi.unisa.it/informatica-magistrale/didattica/insegnamenti?anno=2017&id=507522">Integrazione Dati su Web</a>
+            per l'anno accademico 2017-2018 tenuto dal Prof. <a href="https://rubrica.unisa.it/persone?matricola=001602">Gennaro Costagliola</a>.
+          </p>
+
+          <p>Le fonti utilizzate per questo sito sono:</p>
+          <ul>
+            <li>gli store online <a href="https://www.amazon.it"><em>Amazon</em></a>, <a href="https://www.kobo.com/it"><em>Kobo</em></a>, <a href="https://books.google.com/?hl=it"><em>Google Libri</em></a> per gli ebook;</li>
+            <li>gli store online <a href="https://www.audible.it"><em>Audible</em></a> e <a href="https://www.ilnarratore.com/it/"><em>ilNarratore</em></a> per gli audiolibri</li>
+            <li><a href="https://www.qlibri.it"><em>QLibri</em></a> per le recensioni.</li>
+          </ul>
+
           <p>Google Libri espone delle <a href="https://developers.google.com/books/">API</a> per recuperare informazioni sui libri mentre per gli altri è stato necessario fare attività di <em>scraping</em> per il recupero dati.</p>
         </div>
       </div>
